adabced80d4d9ba0ba74fa8654ebd3274d4816fa
Mariia <zryaninam@gmail.com>
2014-08-13 16:12:58 +0400

    The cacheMatrix.R file is added

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,21 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	  ## initialize the value of the m (inverse matrix)
+	  m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+	  ## compute the inverse matrix using the 'solve' function
+        setsolve <- function(solve) m <<- solve
+	  ## get the value of the inverse matrix
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+
 }
 
 
@@ -12,4 +27,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+ 	  m <- x$getsolve()
+	  ## if the inverse matrix is computed, it will not be calculated again
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+	  ## if the inverse matrix does not exist yet, it will be calculated
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
